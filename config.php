763e76f392da494cfb1107a0b300d96ae6894edd
merkeb <merkebberhane25@outlook.com>
2026-03-29 15:26:22 +0300

    modified the config.php file

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 $conn = pg_connect("
-host=localhost
+host=dpg-d74helcr85hc73fqek10-a
 port=5432
-dbname=simple
-user=postgres
-password=1234
+dbname=simple_oco2
+user=simple_oco2_user
+password=920TuuIu3Dmpw7PrE794bn6T6EfBPfZq
 ");
 
 if (!$conn) {
